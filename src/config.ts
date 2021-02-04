0f270afba0539dfd380161bc062c64d55401333e
electronfriends <electronfriendsde@gmail.com>
2021-02-04 18:37:52 +0100

    Bump PHP version to 8.0.2

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,7 @@ export default {
         },
         {
             name: 'PHP',
-            version: '8.0.1',
+            version: '8.0.2',
             url: 'https://windows.php.net/downloads/releases/php-{version}-nts-Win32-vs16-x64.zip',
             ignoredFiles: ['extras/']
         }
