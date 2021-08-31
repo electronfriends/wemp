83b7cdb14d073d592740dfeac974f3d94161222f
electronfriends <electronfriendsde@gmail.com>
2021-08-31 18:22:19 +0200

    Bump Nginx from 1.21.1 to 1.21.2

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export default {
     services: [
         {
             name: 'Nginx',
-            version: '1.21.1',
+            version: '1.21.2',
             config: 'conf/nginx.conf',
             ignore: ['conf/', 'html/', 'logs/'],
             url: 'https://nginx.org/download/nginx-{version}.zip'
