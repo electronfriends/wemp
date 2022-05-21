bb39d4a325c1329bc1ef426186f3cebc213568ea
electronfriends <electronfriendsde@gmail.com>
2022-05-21 22:48:54 +0200

    Revert MariaDB back to 10.7.3

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ export default {
         },
         {
             name: 'MariaDB',
-            version: '10.8.3',
+            version: '10.7.3',
             config: 'data/my.ini',
             url: 'https://archive.mariadb.org/mariadb-{version}/winx64-packages/mariadb-{version}-winx64.zip'
         },
