0df9c5e06658c2adca065651a49a346b21c58af4
electronfriends <electronfriendsde@gmail.com>
2023-02-02 22:25:34 +0100

    Move to Vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron'
+
+export default defineConfig({
+  plugins: [
+    electron({
+      entry: 'src/main.ts',
+      onstart: options => {
+        // Start Electron App
+        options.startup(['.', '--no-sandbox'])
+      },
+    }),
+  ],
+})
