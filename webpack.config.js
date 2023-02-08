127168dfe1eed436d815dd3f96861b0b0b21f729
electronfriends <electronfriendsde@gmail.com>
2023-02-08 21:51:11 +0100

    Revert to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  target: 'electron-main',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+}
