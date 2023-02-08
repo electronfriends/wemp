ac4dadef434fcdcc5f8aa2092d6f7c5a8ee938d4
electronfriends <electronfriendsde@gmail.com>
2023-02-08 22:26:40 +0100

    Bump phpMyAdmin from 5.2.0 to 5.2.1

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export default {
     },
     {
       name: 'phpMyAdmin',
-      version: '5.2.0',
+      version: '5.2.1',
       config: 'config.inc.php',
       interface: true,
       url: 'https://files.phpmyadmin.net/phpMyAdmin/{version}/phpMyAdmin-{version}-all-languages.zip'
