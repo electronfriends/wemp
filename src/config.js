6a40f5801f6f1faaffbbb580f5871688a419edb5
electronfriends <electronfriendsde@gmail.com>
2024-02-14 21:59:43 +0100

    Bump MariaDB from 11.2.2 to 11.2.3

--- src/config.js
+++ src/config.js
@@ -22,7 +22,7 @@ export default {
     },
     {
       name: 'MariaDB',
-      version: '11.2.2',
+      version: '11.2.3',
       config: 'data/my.ini',
       url: 'https://archive.mariadb.org/mariadb-{version}/winx64-packages/mariadb-{version}-winx64.zip'
     },
