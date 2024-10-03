6122b27377e47b95b6d167807c6671d9673aa029
electronfriends <electronfriendsde@gmail.com>
2024-10-03 19:30:47 +0200

    Revert changes to Vite configs

--- vite.main.config.js
+++ vite.main.config.js
@@ -1,4 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vite';
+import {
+  getBuildConfig,
+  getBuildDefine,
+  external,
+  pluginHotRestart,
+} from './vite.base.config';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig((env) => {
+  /** @type {import('vite').ConfigEnv<'build'>} */
+  const forgeEnv = env;
+  const { forgeConfigSelf } = forgeEnv;
+  const define = getBuildDefine(forgeEnv);
+  const config = {
+    build: {
+      lib: {
+        entry: forgeConfigSelf.entry,
+        fileName: () => '[name].js',
+        formats: ['es'],
+      },
+      rollupOptions: {
+        external,
+      },
+    },
+    plugins: [pluginHotRestart('restart')],
+    define,
+    resolve: {
+      // Load the Node.js entry.
+      mainFields: ['module', 'jsnext:main', 'jsnext'],
+    },
+  };
+
+  return mergeConfig(getBuildConfig(forgeEnv), config);
+});
