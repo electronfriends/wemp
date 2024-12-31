ff72cd8023882f99cf17c23b32284450d6e20f54
electronfriends <electronfriendsde@gmail.com>
2025-01-01 00:45:19 +0100

    Add Fuses to Forge config

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,6 @@
+import { FusesPlugin } from '@electron-forge/plugin-fuses';
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
+
 export default {
   packagerConfig: {
     asar: true,
@@ -44,5 +47,16 @@ export default {
         renderer: [],
       },
     },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: false,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
   ],
 };
