4e6ca7dbdb05e72a1d87efbd34d374481c91ba19
electronfriends <git@electronfriends.org>
2025-10-10 03:04:24 +0200

    Fix packagerConfig by adding platform specification

--- forge.config.js
+++ forge.config.js
@@ -5,7 +5,7 @@ module.exports = {
   packagerConfig: {
     asar: true,
     icon: 'build/icon',
-    extraResource: ['./stubs'],
+    platform: 'win32',
     arch: 'x64',
   },
   rebuildConfig: {},
